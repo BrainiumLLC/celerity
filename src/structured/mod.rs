cfb0de81a36fbbcab08965748f668607b67b72de
luke <luke@brainiumstudios.com>
2021-07-23 11:52:21 -0700

    Remove non-existant path animation from structured::mod.rs.

--- src/structured/mod.rs
+++ src/structured/mod.rs
@@ -1,9 +1,8 @@
 pub mod affine;
 pub mod clock;
 pub mod flip;
-pub mod path;
 pub mod radial;
 pub mod retarget;
 pub mod transform;
 
-pub use self::{affine::*, clock::*, flip::*, path::*, radial::*, retarget::*, transform::*};
+pub use self::{affine::*, clock::*, flip::*, radial::*, retarget::*, transform::*};
