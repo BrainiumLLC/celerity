c21c2ec5b2869715bda6721cd7127fcbf8753054
luke <luke@brainiumstudios.com>
2023-03-14 14:17:21 -0700

    Added BezierCurve struct containing four control points.

--- src/spline/bezier_path.rs
+++ src/spline/bezier_path.rs
@@ -21,3 +21,20 @@ impl<V: Animatable> BezierPath<V> {
         cubic_bezier(b0, &self.b1, &self.b2, b3, t)
     }
 }
+
+pub struct BezierCurve<V: Animatable> {
+    pub b0: V,
+    pub b1: V,
+    pub b2: V,
+    pub b3: V,
+}
+
+impl<V: Animatable> BezierCurve<V> {
+    pub fn new(b0: V, b1: V, b2: V, b3: V) -> Self {
+        Self { b0, b1, b2, b3 }
+    }
+
+    pub fn position(&self, t: f64) -> V {
+        cubic_bezier(&self.b0, &self.b1, &self.b2, &self.b3, t)
+    }
+}
